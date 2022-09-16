e1d9712c3f2ab6bf09fec7bb7d875f957490b5cf
李东华 <lidonghua@polyv.net>
2022-09-16 16:26:03 +0800

    注释

--- TreeTrie.h
+++ TreeTrie.h
@@ -15,8 +15,14 @@ public:
     TreeTrie();
     ~TreeTrie();
 public:
+    // 添加敏感词
     void AddSensitiveWord(std::wstring words);
+    // 删除敏感词
     void DelSensitiveWord(std::wstring words);
+    /*
+    检索敏感词
+    返回一个标记所有敏感词位置的map, map的key为敏感词的起点，value为敏感词的结束位置
+    */
     std::unordered_map<int, int> filter(std::wstring text);
 private:
     void destroy(TreeNode* node);
